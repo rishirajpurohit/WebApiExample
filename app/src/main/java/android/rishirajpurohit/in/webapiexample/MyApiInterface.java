46dcc281510208d70bb941534318b4a21a8a4060
Rishiraj Purohit <purohitji007@gmail.com>
2018-06-29 15:24:48 +0530

    cxxc

--- app/src/main/java/android/rishirajpurohit/in/webapiexample/MyApiInterface.java
+++ app/src/main/java/android/rishirajpurohit/in/webapiexample/MyApiInterface.java
@@ -2,7 +2,8 @@ package android.rishirajpurohit.in.webapiexample;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-
+//https://www.mockable.io
+//https://www.androidhive.info/2016/05/android-working-with-retrofit-http-library/
 /**
  * Created by rishi on 29-06-2018.
  */
